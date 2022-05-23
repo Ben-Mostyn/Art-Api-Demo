22c8275a8a82a90ac44bec8bdbe3ab63e649040c
Ben-Mostyn <ben_mostyn@live.co.uk>
2022-05-23 16:51:35 +0100

    Api Fetch successful

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Artwork from "./components/artwork";
+import { faker } from "@faker-js/faker";
 
 function App() {
+  const [imageId, setImagieId] = useState([]);
+  const [iiifUrl, setIiifUrl] = useState([]);
+  const [error, setError] = useState({
+    error: false,
+    message: "",
+  });
+  const addFakerData = (data) => {
+    let fakerData = faker.commerce.price((10, 20));
+    console.log(fakerData, "i am faker data");
+    // try {
+    //   for (let i = 0; i < data.length; i++) {
+    //     data[i].price = faker.commerce.price();
+    //   }
+    //   return data;
+    //   console.log(data, "i am faker data");
+    // } catch (error) {
+    //   console.log(error, "i am error");
+    // }
+  };
+
+  useEffect(() => {
+    // ! calling the function
+    addFakerData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading">
+        <h1>Welcome to our art store</h1>
+      </div>
+
+      <Artwork
+        imageId={imageId}
+        setImageId={setImagieId}
+        iiifUrl={iiifUrl}
+        setIiifUrl={setIiifUrl}
+        setError={setError}
+      />
     </div>
   );
 }
